4ab94d53fac1ab1d6a8b1993c95685d8ebab73a7
Eremeow138 <60056105+Eremeow138@users.noreply.github.com>
2020-09-28 15:13:20 +0800

    Лэнд закончили, начинаем адаптацию

---
+++ js/icon_customImage.js
@@ -0,0 +1,33 @@
+ymaps.ready(function () {
+    var myMap = new ymaps.Map('yandexMap', {
+            center: [52.250703, 104.281353],
+            zoom: 17
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Собственный значок метки',
+            balloonContent: 'Это красивая метка'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/houseVOP.png',
+            // Размеры метки.
+            iconImageSize: [66, 13],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+
+});
